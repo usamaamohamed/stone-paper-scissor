3bbad7a5c280185a6fd0c00d08dcddee00080961
osama aziz <usamaamohamed378@gmail.com>
2023-11-18 00:10:37 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -16,9 +16,18 @@ int raed_rounds_number(int& number_of_rounds)
     cout << "How Many Rounds You Want To Play? ";
     cin >> number_of_rounds;
 
+    while (cin.fail())     
+    { 
+        // user didn't input a number    
+        cin.clear();      
+        cin.ignore( std::numeric_limits < std::streamsize > ::max(), '\n');          
+        cout << "Invalid data type, Enter a positive number:" << endl;         
+        cin >> number_of_rounds;     
+    } 
+
     while (number_of_rounds <= 0)
     {
-        cout << "number of rounds must be bigger than 0 ,enter howmany rounds you want to play again: ";
+        cout << "Invalid number , Enter a positive number:" << endl; 
         cin >> number_of_rounds;
     }
     return number_of_rounds;
@@ -40,10 +49,23 @@ int  start_round(int& player_choice, int round_number)
 
 enum en_game { stone = 1, paper = 2, scissor = 3 };
 
-string convert_choose(int choice)
+string convert_player_choose(int player_choice)
 {
 
-    switch (choice)
+    switch (player_choice)
+    {
+    case en_game::stone:
+        return "STONE";
+    case en_game::paper:
+        return "PAPER";
+    case en_game::scissor:
+        return "SCISSOR";
+    }
+}
+
+string convert_cmputer_choose(int cmputer_choice)
+{
+    switch (cmputer_choice)
     {
     case en_game::stone:
         return "STONE";
@@ -59,31 +81,31 @@ void round_result(int player_choice, int computer_choice)
     cout << endl;
     if (player_choice == computer_choice)
     {
-        cout << "       YOUR CHOICE IS " << convert_choose(player_choice) << "\n";
-        cout << "       COMPUTER CHOICE IS " << convert_choose(computer_choice) << "\n";
+        cout << "       YOUR CHOICE IS " << convert_player_choose(player_choice) << "\n";
+        cout << "       COMPUTER CHOICE IS " << convert_cmputer_choose(computer_choice) << "\n";
         cout << "       Result is DRAW\n";
         //system("color 1F"); //turn screen to yellow
-        cout << "\a"; //make alert sound
+        //cout << "\a"; //make alert sound
     }
-    else if ( (( player_choice == en_game::paper)   &&  (computer_choice == en_game::stone   )) ||
-              (( player_choice == en_game::stone)   &&  (computer_choice == en_game::scissor )) ||
-              (( player_choice == en_game::scissor) &&  (computer_choice == en_game::paper   )) )
+    else if (((player_choice == en_game::paper) && (computer_choice == en_game::stone)) ||
+        ((player_choice == en_game::stone) && (computer_choice == en_game::scissor)) ||
+        ((player_choice == en_game::scissor) && (computer_choice == en_game::paper)))
     {
-        cout << "       YOUR CHOICE IS " << convert_choose(player_choice) << "\n";
-        cout << "       COMPUTER CHOICE IS " << convert_choose(computer_choice) << "\n";
+        cout << "       YOUR CHOICE IS " << convert_player_choose(player_choice) << "\n";
+        cout << "       COMPUTER CHOICE IS " << convert_cmputer_choose(computer_choice) << "\n";
         cout << "       YOU WIN \n";
         //system("color 2F"); //turn screen to green
-        cout << "\a"; //make alert sound
+        //cout << "\a"; //make alert sound
     }
-    else if ( ((computer_choice == en_game::paper)   &&  (player_choice == en_game::stone   )) ||
-              ((computer_choice == en_game::stone)   &&  (player_choice == en_game::scissor )) ||
-              ((computer_choice == en_game::scissor) &&  (player_choice == en_game::paper   )) )
+    else if (((computer_choice == en_game::paper) && (player_choice == en_game::stone)) ||
+        ((computer_choice == en_game::stone) && (player_choice == en_game::scissor)) ||
+        ((computer_choice == en_game::scissor) && (player_choice == en_game::paper)))
     {
-        cout << "       YOUR CHOICE IS " << convert_choose(player_choice) << "\n";
-        cout << "       COMPUTER CHOICE IS " << convert_choose(computer_choice) << "\n";
+        cout << "       YOUR CHOICE IS " << convert_player_choose(player_choice) << "\n";
+        cout << "       COMPUTER CHOICE IS " << convert_cmputer_choose(computer_choice) << "\n";
         cout << "       COMPUTER WINS \n";
         //system("color 4F"); //turn screen to red
-        cout << "\a"; //make alert sound
+        //cout << "\a"; //make alert sound
     }
     cout << endl;
 }
@@ -101,20 +123,21 @@ void game(int number_of_rounds, int& computer_choice, int player_choice)
 
 }
 
-void start_restart_game(int &number_of_rounds, int &computer_choice, int &player_choice )
+void start_restart_game(int& number_of_rounds, int& computer_choice, int& player_choice)
 {
-    string play_again = "";
+    string x = "";
     do
     {
         raed_rounds_number(number_of_rounds);
-        game(number_of_rounds, computer_choice, player_choice); 
+        game(number_of_rounds, computer_choice, player_choice);
+        
         do
         {
             cout << "GAME HAS ENDED ,DO YOU WANT TO PLAY AGAIN ? [ Y or N ]: \n";
-            cin >> play_again;
-        } while ( (play_again != "Y" ) && ( play_again != "y" ) && ( play_again != "N" ) && ( play_again != "n" ) );
-        //system("color 0F"); //turn screen to black
-    } while (play_again == "y" || play_again == "Y");
+            cin >> x;
+        } while ((x != "Y") && (x != "y") && (x != "N") && (x != "n"));
+        system("color 0F"); //turn screen to black
+    } while (x == "y" || x == "Y");
 
     cout << "           GOOD BYE";
 }
@@ -127,6 +150,6 @@ int main()
     int computer_choice = 0;
     int player_choice = 0;
 
-    start_restart_game(number_of_rounds, computer_choice, player_choice );
+    start_restart_game(number_of_rounds, computer_choice, player_choice);
     return 0;
 }
